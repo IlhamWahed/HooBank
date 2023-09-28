f2977d0b47ff92fdf134f04858e5fa91948cc24c
ilhamwahid <ilham.wahid06@gmail.com>
2023-09-28 21:13:30 +0700

    fect: cardDeal done

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import styles, { layout } from "../style";
+import { card } from "../assets";
+import Button from "./Button";
 
 const CardDeal = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="h-full w-full" />
+      </div>
+    </section>
   );
 };
 
